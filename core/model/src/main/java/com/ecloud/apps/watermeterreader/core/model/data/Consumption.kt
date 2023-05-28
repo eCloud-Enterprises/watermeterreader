dea64306d089f2131cbb52f425ac72e44e128329
Warren Layson <warrenlayson+github@gmail.com>
2023-05-28 20:47:13 +0800

    update model module

---
+++ core/model/src/main/java/com/ecloud/apps/watermeterreader/core/model/data/Consumption.kt
@@ -0,0 +1,11 @@
+package com.ecloud.apps.watermeterreader.core.model.data
+
+data class Consumption(
+    val meterNo: String,
+    val previousReading: Int,
+    val currentReading: Float,
+    val adjustments: Float,
+    val remarks: String,
+    val consumption: Float,
+    val projectCode: String
+)
--- core/model/src/main/java/com/ecloud/apps/watermeterreader/core/model/data/ProjectWithConsumptions.kt
+++ core/model/src/main/java/com/ecloud/apps/watermeterreader/core/model/data/ProjectWithConsumptions.kt
@@ -1,6 +1,7 @@
 package com.ecloud.apps.watermeterreader.core.model.data
 
-data class Project(
+data class ProjectWithConsumptions(
     val code: String,
     val name: String,
+    val consumptions: List<Consumption>
 )
